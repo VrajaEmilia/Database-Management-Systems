0ce589f78ca035f95cbd89c6ca4f610cde7f50c3
VrajaEmilia <100709158+VrajaEmilia@users.noreply.github.com>
2022-10-05 15:58:32 +0300

    lab1/lab2

---
+++ Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvParent = new System.Windows.Forms.DataGridView();
+            this.dgvChild = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvParent)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvChild)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvParent
+            // 
+            this.dgvParent.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvParent.Location = new System.Drawing.Point(12, 22);
+            this.dgvParent.Name = "dgvParent";
+            this.dgvParent.RowHeadersWidth = 51;
+            this.dgvParent.RowTemplate.Height = 24;
+            this.dgvParent.Size = new System.Drawing.Size(521, 394);
+            this.dgvParent.TabIndex = 0;
+            // 
+            // dgvChild
+            // 
+            this.dgvChild.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvChild.Location = new System.Drawing.Point(563, 22);
+            this.dgvChild.Name = "dgvChild";
+            this.dgvChild.RowHeadersWidth = 51;
+            this.dgvChild.RowTemplate.Height = 24;
+            this.dgvChild.Size = new System.Drawing.Size(489, 394);
+            this.dgvChild.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(388, 443);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(327, 55);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "update";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1068, 531);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dgvChild);
+            this.Controls.Add(this.dgvParent);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvParent)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvChild)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvParent;
+        private System.Windows.Forms.DataGridView dgvChild;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab2
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection dbConn;
+        SqlDataAdapter daParent, daChild;
+        DataSet ds = new DataSet();
+        SqlCommandBuilder cb,cbP;
+        BindingSource bsParent, bsChild;
+        string parent = ConfigurationManager.AppSettings.Get("parent");
+        string child = ConfigurationManager.AppSettings.Get("child");
+        string fk_col = ConfigurationManager.AppSettings.Get("fk_column");
+        string fk_rel = ConfigurationManager.AppSettings.Get("fk_relation");
+        public Form1()
+        {
+
+            InitializeComponent();
+            string conStr = ConfigurationManager.AppSettings.Get("conStr");
+            dbConn = new SqlConnection(conStr);
+
+            daParent = new SqlDataAdapter("SELECT * FROM "+parent, dbConn);
+            daChild = new SqlDataAdapter("SELECT * FROM "+child, dbConn);
+            cb = new SqlCommandBuilder(daChild);
+            cbP = new SqlCommandBuilder(daParent);
+            daParent.Fill(ds, parent);
+            daChild.Fill(ds, child);
+            DataRelation dr = new DataRelation(fk_rel,
+            ds.Tables[parent].Columns[fk_col],
+            ds.Tables[child].Columns[fk_col]);
+            ds.Relations.Add(dr);
+            //data binding
+            bsParent = new BindingSource();
+            bsParent.DataSource = ds;
+            bsParent.DataMember = parent;
+            bsChild = new BindingSource();
+            bsChild.DataSource = bsParent;
+            bsChild.DataMember = fk_rel;
+            dgvParent.DataSource = bsParent;
+            dgvChild.DataSource = bsChild;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                daChild.Update(ds, child);
+                daParent.Update(ds, parent);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
